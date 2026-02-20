7e30fe13d9483031580b273fe42d7fbf13d5388a
Rogue Druid <contact@roguedruid.com>
2026-02-20 09:11:35 -0800

    initial

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello World"
+
+ln -sf ~/dotfiles/environment/theme.conf ~/.config/environment.d/theme.conf
+ln -sf ~/dotfiles/waybar ~/.config/
+ln -sf ~/dotfiles/wofi ~/.config
+ln -sf ~/dotfiles/profile/.profile ~/.profile
+ln -sf ~/dotfiles/hypr ~/.config/
