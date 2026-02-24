59f45c290205b73fbc03b7c8cd91b6a4039389dc
Rogue Druid <contact@roguedruid.com>
2026-02-24 08:15:40 -0800

    terminator/starship

--- install.sh
+++ install.sh
@@ -1,11 +1,105 @@
 #!/bin/bash
 
-echo "Hello World"
+update_links() {
+    echo "Updating dotfile symlinks..."
 
-ln -sf ~/dotfiles/environment/theme.conf ~/.config/environment.d/theme.conf
-ln -sf ~/dotfiles/waybar ~/.config/
-ln -sf ~/dotfiles/wofi ~/.config
-# ln -sf ~/dotfiles/profile/.profile ~/.profile
-ln -sf ~/dotfiles/hypr ~/.config/
-ln -sf ~/dotfiles/rofi ~/.config
-ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
+    ln -sf ~/dotfiles/environment/theme.conf ~/.config/environment.d/theme.conf
+    ln -sf ~/dotfiles/waybar ~/.config/
+    ln -sf ~/dotfiles/wofi ~/.config/
+    # ln -sf ~/dotfiles/profile/.profile ~/.profile
+    ln -sf ~/dotfiles/hypr ~/.config/
+    ln -sf ~/dotfiles/rofi ~/.config/
+    # ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
+    ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+    ln -sf ~/dotfiles/ghostty ~/.config/
+    ln -sf ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+    ln -sf ~/dotfiles/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+    ln -sf ~/dotfiles/nvim/ ~/.config/nvim
+    echo "Symlinks updated."
+}
+
+install_zsh() {
+    echo "Installing zsh plugins..."
+
+    local base_dir="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins"
+
+    install_plugin \
+        "https://github.com/zsh-users/zsh-syntax-highlighting.git" \
+        "$base_dir/zsh-syntax-highlighting"
+
+    install_plugin \
+        "https://github.com/zsh-users/zsh-autosuggestions" \
+        "$base_dir/zsh-autosuggestions"
+
+    install_plugin \
+        "https://github.com/zsh-users/zsh-completions.git" \
+        "$base_dir/zsh-completions"
+
+    install_plugin \
+        "https://github.com/zsh-users/zsh-history-substring-search" \
+        "$base_dir/zsh-history-substring-search"
+
+
+
+    echo "Done."
+}
+
+install_all() {
+    update_links
+    install_zsh
+}
+
+show_menu() {
+    clear
+    echo "=============================="
+    echo "   Dotfiles Setup Manager"
+    echo "=============================="
+    echo "1) Update symlinks"
+    echo "2) Install zsh plugin"
+    echo "3) Install everything"
+    echo "4) Exit"
+    echo ""
+}
+
+install_plugin() {
+    local repo_url="$1"
+    local target_dir="$2"
+
+    if [ -d "$target_dir/.git" ]; then
+        echo "🔄 Updating $(basename "$target_dir")..."
+        git -C "$target_dir" pull --ff-only
+    elif [ -d "$target_dir" ]; then
+        echo "⚠️  $target_dir exists but is not a git repo. Skipping."
+    else
+        echo "⬇️  Cloning $(basename "$target_dir")..."
+        git clone "$repo_url" "$target_dir"
+    fi
+}
+
+while true; do
+    show_menu
+    read -p "Select an option [1-4]: " choice
+
+    case $choice in
+        1)
+            update_links
+            read -p "Press enter to continue..."
+            ;;
+        2)
+            install_zsh
+            read -p "Press enter to continue..."
+            ;;
+        3)
+            install_all
+            read -p "Press enter to continue..."
+            ;;
+        4)
+            echo "Goodbye!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid option."
+            sleep 1
+            ;;
+    esac
+done
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,11 +1,31 @@
-# If you come from bash you might have to change your $PATH.
+#################
+# Powerlevel10k #
+#################
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_THEME="spaceship"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
-plugins=(git)
+plugins=(
+    aliases
+    archlinux
+    colored-man-pages
+    git
+    zsh-history-substring-search
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
 
-source $ZSH/oh-my-zsh.sh
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+autoload -U compinit && compinit
+source "$ZSH/oh-my-zsh.sh"
+
+# eval "$(starship init zsh)"
